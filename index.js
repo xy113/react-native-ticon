51cfd7c379a81f8a8dc2b77dc4f788d12b238020
xy113 <songdewei@163.com>
2019-01-28 00:26:04 +0800

    fixed bugs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {TouchableOpacity, Image} from 'react-native';
+import PropType from 'prop-types';
 import {Map} from './map';
 
 export class Ticon extends React.Component<{
-    size:30,
-    onPress:()=>null,
-    color:'#333',
-    name:'blank'
-}>{
+    size?: ?PropType.number,
+    onPress?: ?PropType.func,
+    color?: ?PropType.string,
+    name?: ?PropType.string,
+    style?: ?PropType.object
+}> {
+    static defaultProps = {
+        size: 30,
+        onPress: () => null,
+        color: '#333',
+        name: 'blank',
+        style: {}
+    };
 
     render() {
         const {size, color, name} = this.props;
@@ -15,17 +24,17 @@ export class Ticon extends React.Component<{
             <TouchableOpacity
                 onPress={this.props.onPress}
                 style={{
-                    width:size,
-                    height:size,
+                    width: size,
+                    height: size,
                     ...this.props.style
                 }}
                 activeOpacity={1}
             >
                 <Image style={{
-                    tintColor:color,
-                    width:size,
-                    height:size,
-                    resizeMode:'contain'
+                    tintColor: color,
+                    width: size,
+                    height: size,
+                    resizeMode: 'contain'
                 }} source={Map[name]}/>
             </TouchableOpacity>
         );
